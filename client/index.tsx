6cba6c87d01eaf560b6e5bb455776d379d37c146
Tem Pearse <tem.pearse01@gmail.com>
2023-09-10 12:12:17 +1200

    added index.tsx clientside

---
+++ client/index.tsx
@@ -0,0 +1,31 @@
+import { createRoot } from 'react-dom/client'
+import { Suspense, lazy, useEffect } from 'react'
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+export const routes = createRoutesFromElements(
+  <Route>
+    <Route path="/" element={<App />} />
+    <Route index element={<LandingPage />} />
+  </Route>
+)
+
+function AppProvider() {
+  return <RouterProvider router={createBrowserRouter(routes)} />
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const queryClient = new QueryClient()
+  createRoot(document.getElementById('app') as HTMLElement).render(
+    <QueryClientProvider client={queryClient}>
+      <Suspense>
+        <AppProvider />
+      </Suspense>
+    </QueryClientProvider>
+  )
+})
